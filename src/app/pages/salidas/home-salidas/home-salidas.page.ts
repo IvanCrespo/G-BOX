a479f129c372f9713dd18dc9163e9bc0fee1ef68
IvanCrespo <contacto.ivancrespo@gmail.com>
2022-01-26 12:42:22 -0600

    Home Salidas funcional 90%

--- src/app/pages/salidas/home-salidas/home-salidas.page.ts
+++ src/app/pages/salidas/home-salidas/home-salidas.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BarcodeScanner, BarcodeScannerOptions } from '@awesome-cordova-plugins/barcode-scanner/ngx';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController, ToastController, LoadingController } from '@ionic/angular';
+import { InventariosService } from 'src/app/services/inventarios.service';
+import { NuevaSalidaPage } from '../nueva-salida/nueva-salida.page';
 
 @Component({
   selector: 'app-home-salidas',
@@ -9,14 +11,27 @@ import { NavController } from '@ionic/angular';
 })
 export class HomeSalidasPage implements OnInit {
 
+  /* URL Services */
+  private url = 'salidas';
+
   scannedData: any;
   formatoData: any;
   textQR: any;
+  timer: any;
+
+  /* Data LocalStorage */
+  token: any;
 
   constructor(
     private scanner: BarcodeScanner,
-    private navCtrl: NavController
-  ) { }
+    private navCtrl: NavController,
+    private inventarioServ: InventariosService,
+    private modalController: ModalController,
+    private toastCtrl: ToastController,
+    public loadingCtrl: LoadingController
+  ) {
+    this.token = localStorage.getItem('s_token');
+  }
 
   ngOnInit() {
   }
@@ -43,8 +58,76 @@ export class HomeSalidasPage implements OnInit {
     });
   }
 
-  search() {
-    this.navCtrl.navigateForward(['/nueva-salida']);
+  async checkRealTime(folio_prerequisicion) {
+    if (this.timer) clearTimeout(this.timer);
+    let self = this;
+    this.timer = setTimeout(function () {
+      console.log(folio_prerequisicion);
+      self.inventarioServ.GetAll(self.token, self.url).subscribe(
+        (res: any) => {
+          if (res.status == "fail") {
+            return false;
+          }
+          else {
+            self.buscarPrerequisicion(folio_prerequisicion);
+          }
+        },
+        (err: any) => {
+          console.log('Error', err.message);
+        }
+      )
+    }, 1000);
   }
 
+  async buscarPrerequisicion(folio: string) {
+    this.inventarioServ.GetAll(this.token, `pre_requisicion?s_folio=${folio}`).subscribe((res: any) => {
+      console.log(res);
+      if (!this.isNotErrorApiResponse(res)) {
+        this.presentToast(res.message, "danger", 3000);
+        return false;
+      }
+      else if (res.data.pre_requisicion.length > 1) {
+        this.presentToast(`Ingrese un folio de pre-requisicion`, "warning", 3000);
+        return false;
+      }
+      else if (res.data.pre_requisicion.id_estatus_pre_requisicion != 2) {
+        this.presentToast(`Pre-requisición no autorizada, Autorice la Pre-requisición para poder continuar`, "warning", 3000);
+        return false;
+      }
+      else {
+        console.log('Entra a modal');
+        let data = res.data.pre_requisicion;
+        this.presentToast(`Folio aceptado`, res.status, 3000);
+        this.modalNuevaSalida(data);
+      }
+    })
+  }
+
+  async modalNuevaSalida(data: any) {
+    let prerequisicion = data;
+    const modal = await this.modalController.create({
+      component: NuevaSalidaPage,
+      componentProps: { prerequisicion: prerequisicion }
+    });
+    return await modal.present();
+  }
+
+  /* Errores APIS Status */
+  isNotErrorApiResponse(response: any): boolean {
+    if (response.status == 'empty') return false;
+    if (response.status == 'fail') return false;
+    return true;
+  }
+
+  /* Mostrar Mensaje Toast */
+  async presentToast(message: string, color: string, duration: number) {
+    const toast = await this.toastCtrl.create(
+      {
+        message,
+        color,
+        duration
+      }
+    );
+    toast.present();
+  }
 }
