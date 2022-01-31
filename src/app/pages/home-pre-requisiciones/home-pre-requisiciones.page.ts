e3e7d500258c4cb20e1286d873fb6b1c5fda1e83
IvanCrespo <contacto.ivancrespo@gmail.com>
2022-01-31 13:48:50 -0600

    Corrección de errores Home Pre-requisición 100%

--- src/app/pages/home-pre-requisiciones/home-pre-requisiciones.page.ts
+++ src/app/pages/home-pre-requisiciones/home-pre-requisiciones.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {
   NavController,
   LoadingController,
-  AlertController,
+  AlertController, ToastController
 } from '@ionic/angular';
 import { InventariosService } from 'src/app/services/inventarios.service';
 
@@ -32,7 +32,8 @@ export class HomePreRequisicionesPage implements OnInit {
     private navCtrl: NavController,
     private inventarioServ: InventariosService,
     public loadingCtrl: LoadingController,
-    public alertCtrl: AlertController
+    public alertCtrl: AlertController,
+    private toastCtrl: ToastController
   ) {
     this.token = localStorage.getItem('s_token');
     this.ionViewWillEnter();
@@ -53,7 +54,11 @@ export class HomePreRequisicionesPage implements OnInit {
       this.pre_requisicionesReverse = data.data.pre_requisicion;
       this.pre_requisicionesReverse.reverse();
       this.pre_requisiciones = this.pre_requisicionesReverse;
-      if (data.status == 'fail') {
+      if (!this.isNotErrorApiResponse(data)) {
+        this.presentToast(data.message, "danger", 2500);
+        return false;
+      }
+      else if (data.status == 'fail') {
         this.presentAlert(data.title, data.message);
         loading.dismiss();
       } else if (data.status == 'success') {
@@ -87,4 +92,27 @@ export class HomePreRequisicionesPage implements OnInit {
     await alert.present();
   }
 
+  /* Errores APIS Status */
+  isNotErrorApiResponse(response: any): boolean {
+    if (response.status == 'empty') return false;
+    if (response.status == 'fail') return false;
+    if (response.status == 'logout') {
+      localStorage.clear();
+      this.navCtrl.navigateRoot("/login");
+      return false;
+    }
+    return true;
+  }
+  
+  /* Mostrar Mensaje Toast */
+  async presentToast(message: string, color: string, duration: number) {
+    const toast = await this.toastCtrl.create(
+      {
+        message,
+        color,
+        duration
+      }
+    );
+    toast.present();
+  }
 }
